f33a5d2fdbdf3cfb959f668a90dad0d494fe6dc1
peter <sungjae89@gmail.com>
2023-01-14 23:25:02 +0900

    feat: spring cloud gateway test

---
+++ second-service/src/main/java/com/example/secondservice/controller/SecondController.java
@@ -0,0 +1,17 @@
+package com.example.secondservice.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * Created by peter on 2023/01/14
+ */
+@RestController
+@RequestMapping("/")
+public class SecondController {
+    @GetMapping("/welcome")
+    public String welcome() {
+        return "Welcome to the SecondService";
+    }
+}
