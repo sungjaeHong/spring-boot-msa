d48945f1532e77b6f72d0265ebd14586c8fc21f8
peter <sungjae89@gmail.com>
2023-01-24 21:01:08 +0900

    feat: add message endpoint

--- first-service/src/main/java/com/example/firstservice/controller/FirstController.java
+++ first-service/src/main/java/com/example/firstservice/controller/FirstController.java
@@ -1,17 +1,26 @@
 package com.example.firstservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by peter on 2023/01/14
  */
+@Slf4j
 @RestController
 @RequestMapping("/first-service")
 public class FirstController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the FirstService";
+    }
+
+    @GetMapping("/message")
+    public String message(@RequestHeader("first-request") String header) {
+        log.info("header : {}", header);
+        return "Hello World in First Service";
     }
 }
--- second-service/src/main/java/com/example/secondservice/controller/SecondController.java
+++ second-service/src/main/java/com/example/secondservice/controller/SecondController.java
@@ -1,17 +1,25 @@
 package com.example.secondservice.controller;
 
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestHeader;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
  * Created by peter on 2023/01/14
  */
+@Slf4j
 @RestController
 @RequestMapping("/second-service")
 public class SecondController {
     @GetMapping("/welcome")
     public String welcome() {
         return "Welcome to the SecondService";
+    }
+    @GetMapping("/message")
+    public String message(@RequestHeader("second-request") String header) {
+        log.info("header : {}", header);
+        return "Hello World in Second Service";
     }
 }
